b41cfda3b7076e8abb603012436fa9066b930f1c
Stephan Ude <ude@mediatis.de>
2023-01-13 16:15:25 +0100

    [TASK] implemented session cleanup scheduler task

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,23 +1,30 @@
 <?php
 
-defined('TYPO3') || die();
-
 use DigitalMarketingFramework\Typo3\Collector\Core\Controller\CollectorController;
+use DigitalMarketingFramework\Typo3\Collector\Core\Scheduler\SessionCleanupTask;
+use DigitalMarketingFramework\Typo3\Collector\Core\Routing\Aspect\TyposcriptValueMapper;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
 
-ExtensionUtility::configurePlugin(
-    'DigitalmarketingframeworkCollector',
-    'AjaxService',
-    [
-        CollectorController::class => 'show',
-    ],
-    // non-cacheable actions
-    [
-        CollectorController::class => 'show',
-    ]
-);
+(function () {
+    ExtensionUtility::configurePlugin(
+        'DigitalmarketingframeworkCollector',
+        'AjaxService',
+        [
+            CollectorController::class => 'show',
+        ],
+        // non-cacheable actions
+        [
+            CollectorController::class => 'show',
+        ]
+    );
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['TyposcriptValueMapper'] =
-    \DigitalMarketingFramework\Typo3\Collector\Core\Routing\Aspect\TyposcriptValueMapper::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['TyposcriptValueMapper'] = TyposcriptValueMapper::class;
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][SessionCleanupTask::class] = [
+        'extension' => 'digitalmarketingframework_collector',
+        'title' => 'Digital Marketing Framework - Collector - Session Cleanup (Bot Protection)',
+        'description' => 'Removes expired entries from the bot-protection tables',
+    ];
+})();

babb40e7a66b864e96f9b603e1f314d7aa6a0e91
Stephan Ude <ude@mediatis.de>
2023-01-13 17:50:23 +0100

    [TASK] implemented frontend javascript interface

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,6 @@
 
 use DigitalMarketingFramework\Typo3\Collector\Core\Controller\CollectorController;
 use DigitalMarketingFramework\Typo3\Collector\Core\Scheduler\SessionCleanupTask;
-use DigitalMarketingFramework\Typo3\Collector\Core\Routing\Aspect\TyposcriptValueMapper;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
@@ -20,11 +19,14 @@ defined('TYPO3') || die();
         ]
     );
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['TyposcriptValueMapper'] = TyposcriptValueMapper::class;
-
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][SessionCleanupTask::class] = [
         'extension' => 'digitalmarketingframework_collector',
         'title' => 'Digital Marketing Framework - Collector - Session Cleanup (Bot Protection)',
         'description' => 'Removes expired entries from the bot-protection tables',
     ];
+
+    // the "map" argument can safely be excluded from the cHash
+    // - because it is filtered in the controller (whitelist)
+    // - and because the whole ajax service of this extension is not cached
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_digitalmarketingframeworkcollector_ajaxservice[map]';
 })();

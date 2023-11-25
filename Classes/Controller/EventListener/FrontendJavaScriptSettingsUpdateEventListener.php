e48087f5e2fe16f641d272545fe7d70108c59a94
Stephan Ude <ude@mediatis.de>
2023-11-25 14:57:16 +0100

    user data ajax call has to use global dmf ajax api

--- Classes/Controller/EventListener/FrontendJavaScriptSettingsUpdateEventListener.php
+++ Classes/Controller/EventListener/FrontendJavaScriptSettingsUpdateEventListener.php
@@ -6,47 +6,73 @@ use DigitalMarketingFramework\Collector\Core\Model\Configuration\CollectorConfig
 use DigitalMarketingFramework\Collector\Core\Model\Configuration\CollectorConfigurationInterface;
 use DigitalMarketingFramework\Core\ConfigurationDocument\ConfigurationDocumentManagerInterface;
 use DigitalMarketingFramework\Core\Exception\DigitalMarketingFrameworkException;
-use DigitalMarketingFramework\Core\GlobalConfiguration\GlobalConfigurationInterface;
 use DigitalMarketingFramework\Typo3\Core\Controller\Event\FrontendJavaScriptSettingsUpdateEvent;
 use DigitalMarketingFramework\Typo3\Collector\Core\Registry\Registry;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 
 class FrontendJavaScriptSettingsUpdateEventListener
 {
-    protected GlobalConfigurationInterface $globalConfiguration;
-
     protected ConfigurationDocumentManagerInterface $configurationDocumentManager;
 
     public function __construct(
         protected Registry $registry,
         protected UriBuilder $uriBuilder
     ) {
-        $this->globalConfiguration = $registry->getGlobalConfiguration();
         $this->configurationDocumentManager = $registry->getConfigurationDocumentManager();
     }
 
     protected function getConfiguration(): CollectorConfigurationInterface
     {
-        $documentIdentifier = $this->globalConfiguration->get('core')['configurationStorage']['defaultConfigurationDocument'] ?? '';
-        if ($documentIdentifier === '') {
-            throw new DigitalMarketingFrameworkException('No default configuration document identifier given');
-        }
-
-        $configurationStack = $this->configurationDocumentManager->getConfigurationStackFromIdentifier($documentIdentifier);
+        $configurationStack = $this->configurationDocumentManager->getDefaultConfigurationStack();
 
         return new CollectorConfiguration($configurationStack);
     }
 
-    public function __invoke(FrontendJavaScriptSettingsUpdateEvent $event): void
+    protected function processContentModifiers(CollectorConfigurationInterface $configuration): array
     {
-        $rootPageId = $event->getRootPageId();
-        $configuration = $this->getConfiguration();
-
+        $result = [];
         $contentModifiers = $this->registry->getFrontendContentModifiers($configuration);
         foreach ($contentModifiers as $contentModifier) {
             $keyword = $contentModifier->getKeyword();
             $name = $contentModifier->getContentModifierName();
+            $settings = $contentModifier->getFrontendSettings();
 
+            $result[] = [
+                'type' => 'contentModifier',
+                'plugin' => $keyword . '-' . $name,
+                'settings' => $settings,
+                'action' => 'showContentModifier',
+                'arguments' => ['plugin' => $keyword, 'name' => $name],
+            ];
+        }
+        return $result;
+    }
+
+    /**
+     * @return array<array{type:string,plugin:string,settings:array<string,mixed>,action:string,arguments:array<string,mixed>}
+     */
+    protected function processDataTransformations(CollectorConfigurationInterface $configuration): array
+    {
+        $result = [];
+        $transformationNames = $this->registry->getPublicDataTransformationNames($configuration);
+        foreach ($transformationNames as $transformationName) {
+            $result[] = [
+                'type' => 'userData',
+                'plugin' => $transformationName,
+                'settings' => [],
+                'action' => 'showUserData',
+                'arguments' => ['map' => $transformationName],
+            ];
+        }
+        return $result;
+    }
+
+    /**
+     * @param array<array{type:string,plugin:string,settings:array<string,mixed>,action:string,arguments:array<string,mixed>} $plugins
+     */
+    protected function addPlugins(FrontendJavaScriptSettingsUpdateEvent $event, int $rootPageId, array $plugins): void
+    {
+        foreach ($plugins as $plugin) {
             $uri = $this->uriBuilder->reset()
                 ->setTargetPageType(1673463849)
                 ->setTargetPageUid($rootPageId)
@@ -54,11 +80,22 @@ class FrontendJavaScriptSettingsUpdateEventListener
                     extensionName: 'DmfCollectorCore',
                     pluginName: 'AjaxService',
                     controllerName: 'Collector',
-                    actionName: 'showContentModifier',
-                    controllerArguments: ['plugin' => $keyword, 'name' => $name]
+                    actionName: $plugin['action'],
+                    controllerArguments: $plugin['arguments']
                 );
-
-            $event->addJavaScriptPlugin('contentModifier', $keyword . '-' . $name, url: $uri, settings: []);
+            $event->addJavaScriptPlugin($plugin['type'], $plugin['plugin'], url: $uri, settings: $plugin['settings']);
         }
+    }
+
+    public function __invoke(FrontendJavaScriptSettingsUpdateEvent $event): void
+    {
+        $rootPageId = $event->getRootPageId();
+        $configuration = $this->getConfiguration();
+
+        $plugins = $this->processContentModifiers($configuration);
+        $this->addPlugins($event, $rootPageId, $plugins);
+
+        $plugins = $this->processDataTransformations($configuration);
+        $this->addPlugins($event, $rootPageId, $plugins);
     }
 }

af4c58cc880acab7be314dbf4668a772d5f18f4c
Stephan Ude <ude@mediatis.de>
2024-05-14 21:19:51 +0200

    MFR-83 refactoring registry collections

---
+++ Classes/Registry/EventListener/RegistryCollectionEventListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DigitalMarketingFramework\Typo3\Collector\Core\Registry\EventListener;
+
+use DigitalMarketingFramework\Core\Registry\RegistryCollectionInterface;
+use DigitalMarketingFramework\Core\Registry\RegistryDomain;
+use DigitalMarketingFramework\Typo3\Collector\Core\Registry\Registry;
+
+class RegistryCollectionEventListener
+{
+    public function __construct(
+        protected Registry $registry,
+    ){
+    }
+
+    public function __invoke(RegistryCollectionInterface $collection): void
+    {
+        $collection->addRegistry(RegistryDomain::COLLECTOR, $this->registry);
+    }
+}

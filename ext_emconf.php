d264633f5070b44421e8a83e842267f053fd16c0
Stephan Ude <ude@mediatis.de>
2024-08-07 13:57:37 +0200

    dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '2.1.0',
+    'version' => '2.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
-            'dmf_core' => '2.0.0-2.99.99',
+            'dmf_core' => '2.1.11-2.99.99',
         ],
         'conflicts' => [
         ],

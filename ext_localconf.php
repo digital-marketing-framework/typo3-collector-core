3f58543ee89e6ddf8bc1b6db0dc34a47fe2c3d90
s-ude <ude@mediatis.de>
2025-06-29 17:37:36 +0200

    [TASK] Storage Interface Updates (MFR-133) (#12)

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,14 +6,6 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
 
-(static function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][SessionCleanupTask::class] = [
-        'extension' => 'dmf_collector_core',
-        'title' => 'Anyrel - Collector - Session Cleanup (Bot Protection)',
-        'description' => 'Removes expired entries from the bot-protection tables',
-    ];
-})();
-
 ExtensionUtility::configurePlugin(
     extensionName: 'DmfCollectorCore',
     pluginName: 'ContentModifier',
